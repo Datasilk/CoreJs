d0770ef99bae1b40e945417bb68ef417b134824b
Mark Entingh <entingh@gmail.com>
2018-01-20 04:47:47 -0800

    fixed S.util.css.load

--- util.js
+++ util.js
@@ -20,7 +20,7 @@
             var link = document.createElement('link');
             link.rel = 'stylesheet';
             link.type = 'text/css';
-            link.src = file;
+            link.href = file;
             link.id = id;
             head.appendChild(link);
         },
