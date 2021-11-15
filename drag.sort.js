b9a7b21f686c13d3318658863d5c5d1acb6d5ff8
Mark Entingh <entingh@gmail.com>
2021-11-15 03:31:46 -0600

    fixed bugs related to drag.sort

--- drag.sort.js
+++ drag.sort.js
@@ -19,21 +19,34 @@ S.drag.sort = {
             let parent = parents[x];
             $(parent).off('dragover').on('dragover', (e) => {
                 e.preventDefault();
-                const afterElement = S.drag.sort.after(elems, e.clientY);
+                const afterElement = S.drag.sort.after(elems, parent, e.clientY);
                 const draggable = parents.find('.dragging')[0];
+                if (!draggable) { return;}
+                console.log(['draggable', draggable]);
                 if (afterElement == null) {
                     parent.appendChild(draggable);
                 } else {
-                    parent.insertBefore(draggable, afterElement);
+                    const box = afterElement.getBoundingClientRect();
+                    if (box.top + (box.height / 2) > e.clientY) {
+                        parent.insertBefore(draggable, afterElement);
+                    } else {
+                        parent.insertAfter(draggable, afterElement);
+                    }
                 }
             });
         }
     },
-    after: function(items, y) {
+    after: function (items, parent, y) {
         const elems = items.filter((i, a) => a.className.indexOf('dragging') < 0);
         return elems.reduce((closest, child) => {
+            if ($(child).parents(parent).length <= 0) { return closest; }
+            if (!child) { return closest; }
             const box = child.getBoundingClientRect();
             const offset = y - box.top - box.height / 2;
+            if (offset == -Infinity || offset == Infinity) {
+                console.log(box);
+                offset = 0;
+            }
             if (closest != null && offset < 0 && offset > closest.offset) {
                 return { offset: offset, element: child };
             } else {
