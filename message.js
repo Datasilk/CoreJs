10344487bf46d48f636b52c5d2c5860555562553
Mark Entingh <entingh@gmail.com>
2022-03-04 01:08:00 -0600

    added clear function used to remove all existing messages

--- message.js
+++ message.js
@@ -6,11 +6,16 @@
         $(element).append('<div class="message ' + (type ? type : '') + '"><span>' + msg + `</span><div class="col right icon msg-close small">
             <svg viewBox="0 0 32 32"><use xlink:href="#icon-close" x="0" y="0" width="32" height="32"></use></svg>
             </div></div>`);
-        $(element + ' .msg-close').on('click', (e) => {
+        $(element).find('.msg-close').on('click', (e) => {
             var target = $(e.target);
             if (!target.hasClass('message')) { target = target.parents('.message').first(); }
             target.remove();
         });
+    },
+
+    clear: function (element) {
+        if (!element) { element = S.message.defaultElement };
+        $(element).html('');
     },
 
     error: {
